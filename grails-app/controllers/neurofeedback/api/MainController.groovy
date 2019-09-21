d0dbad0a47c11022cced3d085454db154a0583a2
Ivanexerufino <ivan-2100@hotmail.com>
2019-09-20 21:00:18 -0300

    not needed

--- grails-app/controllers/neurofeedback/api/MainController.groovy
+++ grails-app/controllers/neurofeedback/api/MainController.groovy
@@ -1,10 +1,5 @@
 package neurofeedback.api
 
-import grails.plugin.springsecurity.annotation.Secured
-import grails.transaction.Transactional
-
-@Transactional(readOnly = true)
-@Secured(['ROLE_ADMIN'])
 class MainController {
 
     def index() {
