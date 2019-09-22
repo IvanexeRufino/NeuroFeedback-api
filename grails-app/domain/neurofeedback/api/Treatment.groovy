19aed0dcfec139d8374215bb2c9921c0d0e441c7
Leandro <leandro.sanchezzarfino@gmail.com>
2019-09-22 13:30:17 -0300

    Update Treatment.groovy

--- grails-app/domain/neurofeedback/api/Treatment.groovy
+++ grails-app/domain/neurofeedback/api/Treatment.groovy
@@ -10,8 +10,12 @@ class Treatment {
         id (unique: true, maxSize: 11)
         name (unique: true, blank: false, maxSize: 50)
         description (blank: false, maxSize: 255)
+        treatmentTimestamp (blank: false,value:now())
+        duration (blank: false, minValue: 0)
+        effectiveness (blank: false, range: 0..100)
     }
+    
     def toJson(){
-        ["name":name,"description":description]
+        ["id":id,"name":name,"description":description,"effectiveness":effectiveness,"duration":duration]
     }
 }
