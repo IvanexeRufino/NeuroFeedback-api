f2cd78ba669e1037ed40f345adfdfd9ebac7a8f8
ivanexerufino <ivan-2100@hotmail.com>
2019-11-06 16:15:01 -0300

    transmissor

--- pythonModules/transmissorClient.py
+++ pythonModules/transmissorClient.py
@@ -19,7 +19,7 @@ def main(argv):
 
     print ('Input file is ', inputfile)
 
-    url = 'http://localhost:8080/trackSession/treatmentSession/' + str(treatment_id)
+    url = 'http://whispering-temple-11733.herokuapp.com/trackSession/treatmentSession/' + str(treatment_id)
 
     payload = []
     headers = {
@@ -50,7 +50,7 @@ def main(argv):
 
             line = fp.readline()
 
-    url = 'http://localhost:8080/trackSession/endTreatment/' + str(treatment_id)
+    url = 'http://whispering-temple-11733.herokuapp.com/trackSession/endTreatment/' + str(treatment_id)
     requests.post(url, headers=headers)
 
 if __name__ == "__main__":
